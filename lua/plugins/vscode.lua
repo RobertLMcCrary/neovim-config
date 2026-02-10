3a52ace6d9b4638e3532b3496c2ce025ab63af85
RobertLMcCrary <rlmccrary1210@gmail.com>
2026-02-10 17:34:26 -0500

    turn light mode off

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -3,8 +3,8 @@ return {
     priority = 1000,
     config = function()
         require("vscode").setup({
-            --style = "dark",
-            style = "light" -- use light mode if bright outside
+            style = "dark",
+            --style = "light" -- use light mode if bright outside
             -- transparent = true
         })
         vim.cmd.colorscheme("vscode")
